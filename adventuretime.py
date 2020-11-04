4b7a6e8fef9419c96e8c7ebcd80cc8bd8f755434
Eric St. John <esj1331@gmail.com>
2020-11-04 19:11:55 +0000

    flow

---
+++ adventuretime.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+  
+
+
+game_start = "yes"
+
+while game_start == "yes":
+        print("You wake up alone in an all white room with no windows. On a table in front of you is a magical loo              king rainbow colored pill. Will you take the pill?")
+        answer_1 = input("yes or no?")
+
+        if answer_1 == "yes":
+            print("""
+You swallow the pill, and your head begins to spin. Your vision gets blurry , and you feel your                  begin to morph and change.  You are getting smaller, and somehow greener?? Finally, its done.
+                You are now outside in a wonderful forest. You have become a frog. You hear a scream in the
+                    distance. Will you hop towards danger?""")
+
+        else:
+            print("DONT BE A NERD! EAT THE MAGICAL RAINBOW PILL")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        game_start = input("Do you want to start over and not be stupid?")
